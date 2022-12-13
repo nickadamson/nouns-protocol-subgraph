d8476ff9a987771a4468aec90066497f8c6e14e1
nickadamson <naburner@protonmail.com>
2022-12-12 17:26:55 -0800

    feat: add utils/constants.ts

---
+++ utils/constants.ts
@@ -0,0 +1,4 @@
+import { Address } from "@graphprotocol/graph-ts";
+
+export const ZERO_ADDRESS_STRING = "0x0000000000000000000000000000000000000000";
+export const ZERO_ADDRESS = Address.fromString(ZERO_ADDRESS_STRING);
